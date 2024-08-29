116044fbb6ae037a401799cbfe4bb16660c6dffa
Agustin <amoralejo@fi.uba.ar>
2024-08-28 22:28:56 -0300

    Agrego selector de provincia y localidades

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,22 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)  # Habilitar CORS para todos los endpoints
 
 # Cargar el modelo
-model = joblib.load('model.pkl')
+#model = joblib.load('model.pkl')
+
+# Cargar el DataFrame del CSV al inicio
+current_dir = os.path.dirname(os.path.abspath(__file__))
+data_dir = 'data'
+filename = 'precios_por_localidad_bs_as.csv'
+filepath = os.path.join(current_dir, data_dir, filename)
+
+df_localidades = pd.read_csv(filepath)
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -18,10 +28,34 @@ def predict():
     precio_m2 = data['precio_m2']
 
     # Realizar la predicción con el modelo cargado
-    prediction = model.predict([[superficie_total, precio_m2_medio_localidad, precio_medio_localidad, precio_m2]])
+ #   prediction = model.predict([[superficie_total, precio_m2_medio_localidad, precio_medio_localidad, precio_m2]])
 
     # Devolver la predicción como JSON
-    return jsonify({'prediction': prediction.tolist()})
+#    return jsonify({'prediction': prediction.tolist()})
+    return 199
+
+@app.route('/api/provincias', methods=['GET'])
+def get_provincias():
+    # Suponiendo que la columna de provincias se llama 'provincia'
+    provincias_unicas = df_localidades['provincia'].unique().tolist()
+    print(provincias_unicas)
+    return jsonify(provincias_unicas)
+
+@app.route('/api/localidades', methods=['GET'])
+def get_localidades():
+    # Obtener el parámetro de consulta 'provincia' de la URL
+    provincia_seleccionada = request.args.get('provincia')
+
+    if provincia_seleccionada:
+        # Filtrar las localidades en base a la provincia seleccionada
+        localidades_filtradas = df_localidades[df_localidades['provincia'] == provincia_seleccionada]
+        localidades = localidades_filtradas.to_dict(orient='records')
+    else:
+        # Si no se pasa ninguna provincia, devolver todas las localidades
+        localidades = df_localidades.to_dict(orient='records')
+
+    return jsonify(localidades)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- packages/nextjs/app/predecirPrecio/PredecirPrecio.tsx
+++ packages/nextjs/app/predecirPrecio/PredecirPrecio.tsx
@@ -1,23 +1,26 @@
 "use client";
 
-import React, { useState, ChangeEvent, FormEvent } from 'react';
+import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react';
 import axios from 'axios';
-import Link from "next/link";
-import { IntegerInput, EtherInput } from "~~/components/scaffold-eth";
+import { IntegerInput } from "~~/components/scaffold-eth";
 
 export const PredecirPrecio = () => {
   const [superficie_total, setSuperficie_total] = useState<string>("");
-  const [precio_m2_medio_localidad, setPrecio_m2_medio_localidad] = useState<string>("");
+  const [superficie_cubierta, setSuperficie_cubierta] = useState<string>("");
   const [precio_medio_localidad, setPrecio_medio_localidad] = useState<string>("");
   const [precio_m2, setPrecio_m2] = useState<string>("");
   const [prediction, setPrediction] = useState<number | null>(null);
+  const [provincias, setProvincias] = useState<any[]>([]);
+  const [selectedProvincia, setSelectedProvincia] = useState<string>("");
+  const [localidades, setLocalidades] = useState<any[]>([]);
+  const [selectedLocalidad, setSelectedLocalidad] = useState<string>("");
 
   const handleSuperficieTotalChange = (value: string | bigint) => {
     setSuperficie_total(String(value));
   };
 
-  const handlePrecioM2MedioLocalidadChange = (value: string | bigint) => {
-    setPrecio_m2_medio_localidad(String(value));
+  const handleSuperficieCubiertaChange = (value: string | bigint) => {
+    setSuperficie_cubierta(String(value));
   };
 
   const handlePrecioMedioLocalidadChange = (value: string | bigint) => {
@@ -30,9 +33,17 @@ export const PredecirPrecio = () => {
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
+    
+    console.log('Provincia seleccionada:', selectedProvincia);
+    console.log('Localidad seleccionada:', selectedLocalidad);
+    console.log('Superficie Total:', superficie_total);
+    console.log('Superficie Cubierta:', superficie_cubierta);
+    console.log('Precio Medio Localidad:', precio_medio_localidad);
+    console.log('Precio m²:', precio_m2);
+      
     axios.post('http://localhost:5000/predict', {
       superficie_total: parseFloat(superficie_total),
-      precio_m2_medio_localidad: parseFloat(precio_m2_medio_localidad),
+      superficie_cubierta: parseFloat(superficie_cubierta),
       precio_medio_localidad: parseFloat(precio_medio_localidad),
       precio_m2: parseFloat(precio_m2),
     })
@@ -44,20 +55,85 @@ export const PredecirPrecio = () => {
     });
   };
 
+  const fetchProvincias = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/api/provincias');
+      const data = await response.json();
+      setProvincias(data);
+    } catch (error) {
+      console.error('Error fetching provincias:', error);
+    }
+  };
+  
+  useEffect(() => {
+    fetchProvincias();
+  }, []);
+  
+  const handleProvinciaChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const selectedProvincia = e.target.value;
+    setSelectedProvincia(selectedProvincia);
+  
+    if (selectedProvincia) {
+      fetchLocalidades(selectedProvincia);
+    }
+  };
+  
+  const fetchLocalidades = async (provincia: string) => {
+    try {
+      const response = await fetch(`http://localhost:5000/api/localidades?provincia=${provincia}`);
+      const data = await response.json();
+      setLocalidades(data);
+    } catch (error) {
+      console.error('Error fetching localidades:', error);
+    }
+  };
+  
+  const handleLocalidadChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedLocalidad(e.target.value);
+  };
+  
   return (
     <div className="px-6 pt-10 pb-8 shadow-xl sm:my-auto bg-secondary sm:mx-auto sm:max-w-11/12 md:w-9/12 sm:w-11/12 sm:rounded-lg sm:px-10">
-      <img src="ARPA-WIDE.png" alt="logo de ARPA" className="mx-auto imgRounder"/>
+      <img src="ARPA-WIDE.png" alt="logo de ARPA" className="mx-auto imgRounder" />
       <h1 className="text-xl font-bold">Ingresá los datos de la propiedad:</h1>
       <br /><br />
       <form onSubmit={handleSubmit}>
+          <label className="text-md font-bold">Provincia</label>
+          <br />
+          <select 
+            value={selectedProvincia} 
+            onChange={handleProvinciaChange} 
+            className="text-md font-bold form-select rounded-full w-full h-8">
+            <option value="">Seleccionar provincia</option>
+            {provincias.map((provincia, index) => (
+              <option key={index} value={provincia}>
+                {provincia}
+              </option>
+            ))}
+          </select>
+          <br />
+          <br />
         <div>
-          <label className="text-md font-bold">Superficie Total</label>
+          <label className="text-md font-bold">Localidad</label>
+          <br />
+          <select value={selectedLocalidad} onChange={handleLocalidadChange} className="text-md font-bold form-select rounded-full w-full h-8">
+            <option value="">Seleccionar localidad</option>
+            {localidades.map((localidad, index) => (
+              <option key={index} value={localidad.localidad}>
+                {localidad.localidad}
+              </option>
+            ))}
+          </select>
+        </div>
+        <br />
+        <div>
+            <label className="text-md font-bold">Superficie Total</label>
           <IntegerInput value={superficie_total} onChange={handleSuperficieTotalChange} placeholder="Ingresar superficie total" />
         </div>
         <br />
         <div>
-          <label className="text-md font-bold">Precio m² Medio Localidad</label>
-          <IntegerInput value={precio_m2_medio_localidad} onChange={handlePrecioM2MedioLocalidadChange} placeholder="Ingresar precio m² medio localidad" />
+          <label className="text-md font-bold">Superficie cubierta</label>
+          <IntegerInput value={superficie_cubierta} onChange={handleSuperficieCubiertaChange} placeholder="Ingresar superficie cubierta" />
         </div>
         <br />
         <div>
