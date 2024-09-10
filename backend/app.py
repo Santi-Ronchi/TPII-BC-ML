b23b8062d38a5fd23dc458e1c32c21b92325b668
Agustin <amoralejo@fi.uba.ar>
2024-09-10 20:16:10 -0300

    probando mejoras en el modelo

--- backend/app.py
+++ backend/app.py
@@ -9,15 +9,22 @@ CORS(app)  # Habilitar CORS para todos los endpoints
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 data_dir = 'data'
-filename = 'precios_por_localidad.csv'
+#filename = 'precios_por_localidad.csv'
+# PRUEBA CON EL ULTIMO MODELO
+filename = 'precios_por_localidad_prov_v2.csv'
 filepath = os.path.join(current_dir, data_dir, filename)
 
-model_path = os.path.join(current_dir, 'modelo_filtrado_v2.pkl')
-scaler_path = os.path.join(current_dir, 'scaler_v2.joblib')
+df_localidades = pd.read_csv(filepath)
+
+model_path = os.path.join(current_dir, 'modelo_filtrado_v1.pkl')
+scaler_path = os.path.join(current_dir, 'scaler_v1.joblib')
 model = joblib.load(model_path)
 scaler = joblib.load(scaler_path)
 
-df_localidades = pd.read_csv(filepath)
+model_path_prov = os.path.join(current_dir, 'modelo_filtrado_prov_v2.pkl')
+scaler_path_prov = os.path.join(current_dir, 'scaler_prov_v2.joblib')
+model_prov = joblib.load(model_path_prov)
+scaler_prov = joblib.load(scaler_path_prov)
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -33,21 +40,24 @@ def predict():
 
     localidad = df_localidades[df_localidades['localidad'] == data['localidad']]
 
+    if data["provincia"] == "Ciudad Autonoma de Buenos Aires":
+        datos_entrada = [[superficie_total, superficie_cubierta, cantidad_baños, cantidad_dormitorios, localidad["precio_m2_medio"].values[0], localidad["precio_m2_medio"].values[0],localidad["precio_medio"].values[0]]]    
+        datos_entrada_escalados = scaler.transform(datos_entrada)
+        prediction = model.predict(datos_entrada_escalados)
+    else:
+        # ANTERIOR AL ULTIMO modelo_filtrado_prov scaler_prov precios_por_localidad_v1
+        #datos_entrada = [[superficie_cubierta,superficie_total,localidad["precio_m2_medio"].values[0],localidad["precio_medio"].values[0],cantidad_ambientes,0,cantidad_dormitorios,cantidad_baños]]    
+        #datos_entrada_escalados = scaler_prov.transform(datos_entrada)
+        #prediction = model_prov.predict(datos_entrada_escalados)
+        
+        # ULTIMA PRUEBA
+        datos_entrada = [[superficie_total,superficie_cubierta,localidad["precio_m2_medio"].values[0],localidad["precio_m2_medio"].values[0],localidad["precio_medio"].values[0]]]    
+        datos_entrada_escalados = scaler_prov.transform(datos_entrada)
+        prediction = model_prov.predict(datos_entrada_escalados)
 
-#   Me parece que es mas razonable la prediccion de V1
-
-#   v1 --> sup_total, sup_cubierta, cant_baños, cant_dorm, precio_m2, precio_m2_medio_localidad, precio_medio_localidad
-#    datos_entrada = [[superficie_total, superficie_cubierta, cantidad_baños, cantidad_dormitorios, localidad["precio_m2_medio"].values[0], localidad["precio_m2_medio"].values[0],localidad["precio_medio"].values[0]]]
-
-#   v2 --> sup_total, precio_m2, precio_m2_medio_localidad, precio_medio_localidad, sup_cubierta, cant_dorm, cant_baños, cant_ambientes
-    datos_entrada = [[superficie_total, localidad["precio_m2_medio"].values[0], localidad["precio_m2_medio"].values[0], localidad["precio_medio"].values[0], superficie_cubierta, cantidad_dormitorios, cantidad_baños, cantidad_ambientes]]
 
 
-    datos_entrada_escalados = scaler.transform(datos_entrada)
-    print("Datos escalados:", datos_entrada_escalados)
-
-    prediction = model.predict(datos_entrada_escalados)
-    print(prediction)
+    print("Prediccion: ", prediction)
     return jsonify({'prediction': prediction.tolist()})
 
 @app.route('/api/provincias', methods=['GET'])
