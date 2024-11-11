dd313a8b5ce1b441fc5572a94f387e45615a7534
josefinacairo <mcairo@fi.uba.ar>
2024-11-11 19:31:53 -0300

    fix

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -36,14 +36,15 @@ contract YourContract {
 	mapping(uint256 => bool) public propiedadesAlquiladas;
 
 	event ContratoCreado(
-		address Owner,
+//		address Owner,
 		uint256 Monto,
 		uint256 ID,
-		string pass,
-		bool fixed_penalty,
+//		string pass,
+//		bool fixed_penalty,
 		uint256 GracePeriod,
 		uint256 Penalty_Percentage,
 		uint256 Duration
+//        uint256 startDate
     );
 
 	struct ContratoAlquiler{
@@ -71,7 +72,7 @@ contract YourContract {
 		contratosAlquiler[_ID] = ContratoAlquiler(block.timestamp,ContractStatus.Draft,msg.sender,address(0),Monto,_ID,allowedWallet,penalty,_Penalty_Percentage, _GracePeriod,_Duration,block.timestamp);
 		propiedadesAlquiladas[_ID] = true;
 
-		emit ContratoCreado(_Owner, Monto, _ID,  _pass,  fixed_penalty,  _GracePeriod,  _Penalty_Percentage,  _Duration);
+		emit ContratoCreado(Monto, _ID,  _GracePeriod,  _Penalty_Percentage,  _Duration);
 
 	}
 
