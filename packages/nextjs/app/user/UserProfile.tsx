267723d8f7c911cfa626ba262f35d1b3f146d7b2
Agustin <amoralejo@fi.uba.ar>
2024-11-18 14:41:08 -0300

    Pagina de usuario login con wallet o email

--- packages/nextjs/app/user/UserProfile.tsx
+++ packages/nextjs/app/user/UserProfile.tsx
@@ -16,7 +16,8 @@ const UserProfile: React.FC<UserProfileProps> = ({ userId }) => {
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const userDoc = doc(db, "Email-Wallets", userId);
+        const tableName = userId.includes("@") ? "Email-Wallets" : "Wallet-email";
+        const userDoc = doc(db, tableName, userId);
         const userSnapshot = await getDoc(userDoc);
 
         if (userSnapshot.exists()) {
@@ -41,9 +42,6 @@ const UserProfile: React.FC<UserProfileProps> = ({ userId }) => {
   return (
     <div className="px-6 pt-10 pb-8 shadow-xl sm:my-auto bg-secondary sm:mx-auto sm:max-w-11/12 md:w-9/12 sm:w-11/12 sm:rounded-lg sm:px-10"
     >
-      <h2 style={{ fontSize: '24px', color: '#333', marginBottom: '10px' }}>
-         {/*user.name  (PORIAMOS POR EJEMPLO PONER EL NOMBRE DEL USUARIO. AGREGAR EN UTILS Y FIREBASE)*/} 
-      </h2>
       <p style={{ fontSize: '16px', color: '#555', marginBottom: '20px' }}>
         Email del usuario: <span style={{ color: '#0073e6' }}>{user.userEmail}</span>
       </p>
@@ -53,27 +51,45 @@ const UserProfile: React.FC<UserProfileProps> = ({ userId }) => {
           Wallets
         </h3>
         
-        {user.walletAddr.length > 0 ? (
-          <ul style={{ padding: '0', listStyle: 'none' }}>
-            {user.walletAddr.map((wallet, index) => (
-              <li
-                key={index}
-                style={{
-                  backgroundColor: '#fff',
-                  padding: '10px',
-                  margin: '10px 0',
-                  borderRadius: '4px',
-                  boxShadow: '0 2px 4px rgba(0, 0, 0, 0.05)',
-                  color: '#333',
-                }}
-              >
-                {JSON.stringify(wallet)}
-              </li>
-            ))}
-          </ul>
+        {Array.isArray(user.walletAddr) ? (
+          user.walletAddr.length > 0 ? (
+            <ul style={{ padding: '0', listStyle: 'none' }}>
+              {user.walletAddr.map((wallet, index) => (
+                <li
+                  key={index}
+                  style={{
+                    backgroundColor: '#fff',
+                    padding: '10px',
+                    margin: '10px 0',
+                    borderRadius: '4px',
+                    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.05)',
+                    color: '#333',
+                  }}
+                >
+                  Dirección: {wallet}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p style={{ color: '#999' }}>No hay wallets asociadas.</p>
+          )
+        ) : user.walletAddr ? (
+          <p
+            style={{
+              backgroundColor: '#fff',
+              padding: '10px',
+              margin: '10px 0',
+              borderRadius: '4px',
+              boxShadow: '0 2px 4px rgba(0, 0, 0, 0.05)',
+              color: '#333',
+            }}
+          >
+            Dirección: {user.walletAddr}
+          </p>
         ) : (
           <p style={{ color: '#999' }}>No hay wallets asociadas.</p>
         )}
+
         
       </div>
 
