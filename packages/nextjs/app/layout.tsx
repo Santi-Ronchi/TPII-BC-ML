56d68ddd5da659523d4787c53ecb707258a4d928
Agustin <amoralejo@fi.uba.ar>
2024-11-22 23:24:46 -0300

    Perfil de usuario carga con login email

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -3,6 +3,7 @@ import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithPro
 import { ThemeProvider } from "~~/components/ThemeProvider";
 import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
+import { UserProvider } from "./user/UserContext";
 
 export const metadata = getMetadata({
   title: "Scaffold-ETH 2 App",
@@ -14,7 +15,9 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
     <html suppressHydrationWarning>
       <body>
         <ThemeProvider enableSystem>
-          <ScaffoldEthAppWithProviders>{children}</ScaffoldEthAppWithProviders>
+          <UserProvider>
+            <ScaffoldEthAppWithProviders>{children}</ScaffoldEthAppWithProviders>
+          </UserProvider>
         </ThemeProvider>
       </body>
     </html>
--- packages/nextjs/app/user/page.tsx
+++ packages/nextjs/app/user/page.tsx
@@ -1,19 +1,25 @@
+'use client';
 import React from 'react';
 import UserProfile from './UserProfile';
 import { NextPage } from 'next';
+import { UserProvider, useUser } from './UserContext';
 
 
 const UserPage: NextPage = () => {
+  const { email } = useUser();
+  
+  if (!email) return <p>Debe iniciar sesion para ver la informacion de usuario.</p>;
+
   return (
-    <><div>
-      <UserProfile userId="0x3aF55197db2a66cd3C48840F6F710594D8ed2e6b" />
+    <><div> 
+      <UserProfile userId={email} />
       { /*
       <UserProfile userId="0x3aF55197db2a66cd3C48840F6F710594D8ed2e6b" />
       
       <UserProfile userId="prueba@gmail.com" />
       */}
     </div><div className="text-center mt-8 bg-secondary p-10">
-        <h1 className="text-4xl my-0">¿Cómo retiro mi dinero?</h1>
+        <h1 className="text-4xl my-0">Dale, ponelo en alquiler</h1>
         <p className="text-neutral">
           Podrás retirar el dinero del contrato a tu billetera conectada cuando un inquilino lo deposite.
           <br /> Con amor,{" "}
